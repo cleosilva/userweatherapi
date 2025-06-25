a4714a2d4a19e8616a60a7317c7d354467bac71c
Cleo Silva <cleo_silva1906@hotmail.com>
2025-06-25 20:14:24 -0300

    build: Iniciando projeto

---
+++ src/main/java/com/cleosilva/userweatherapi/ProjetoUserwheatherapiApplication.java
@@ -0,0 +1,13 @@
+package com.cleosilva.userweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUserwheatherapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUserwheatherapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleosilva/userweatherapi/ProjetoUserwheatherapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleosilva.userweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUserwheatherapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
